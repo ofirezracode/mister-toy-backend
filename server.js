1bfe26820a4bc21aff7a9b0bc18f48d3cd280e98
ofirezracode <ofirezracode@gmail.com>
2023-05-23 15:25:32 +0300

    UPDATE: added frontend

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const path = require('path')
 const cors = require('cors')
 
 // App Configuration
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  const corsOptions = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
+}
 const corsOptions = {
   origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
   credentials: true,
 }
-app.use(cors(corsOptions))
-app.use(express.static('public'))
+
 app.use(cookieParser()) // for res.cookies
 app.use(express.json()) // for req.body
 
@@ -152,8 +160,12 @@ app.delete('/api/toy/:toyId', (req, res) => {
 //   res.send('logged-out!')
 // })
 
+const port = process.env.PORT || 3030
+
 app.get('/**', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(3030, () => console.log('Server listening on port 3030!'))
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
